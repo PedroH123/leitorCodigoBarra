bb2627533c442a84a03beb313cfa8aa99b9d8a7c
PedroH123 <pedro_3ano@hotmail.com>
2019-08-21 02:15:45 +0900

    JavaScript Funcionando

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,2 +1,26 @@
-// This is a JavaScript file
 
+$(document).on("click","#botao",function(){
+    cordova.plugins.barcodeScanner.scan(
+      function (result) {
+            if(result.text == 280720550){
+                 $(location).attr("href", "codigo1.html")
+            }
+      },
+      function (error) {
+          alert("Scanning failed: " + error);
+      },
+      {
+          preferFrontCamera : false, // iOS and Android
+          showFlipCameraButton : true, // iOS and Android
+          showTorchButton : true, // iOS and Android
+          torchOn: false, // Android, launch with the torch switched on (if available)
+          saveHistory: true, // Android, save scan history (default false)
+          prompt : "Place a barcode inside the scan area", // Android
+          resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+          formats : "QR_CODE,PDF_417,DATA_MATRIX,CODABAR,CODE_128	,CODE_93,CODE_39	", // default: all but PDF_417 and RSS_EXPANDED
+          orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+          disableAnimations : true, // iOS
+          disableSuccessBeep: false // iOS and Android
+      }
+   );
+})
